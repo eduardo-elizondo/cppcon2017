02fdcdef1da0390511142cc8aa52b52cb4210162
eduardo-elizondo <eelizondo@fb.com>
2017-09-02 18:08:55 -0700

    Added patches

--- example1/fib80.cpp
+++ example1/fib80.cpp
@@ -1,17 +1,17 @@
 template <long long N>
 struct fib {
   static const int value = fib<N - 1>::value + fib<N - 2>::value;
-}
+};
 
-tempalte <>
+template <>
 struct fib<1> {
   static const int value = 1;
-}
+};
 
 template <>
 struct fib<0> {
   static const int value = 1;
-}
+};
 
 int main() {
   int x = fib<80>::value;
