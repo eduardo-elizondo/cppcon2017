ce772166c673a0008c309a6b7c36bb7d0776488b
eduardo-elizondo <eelizondo@fb.com>
2017-09-03 19:34:45 -0700

    Generate file writer

--- idl_generator/generate.py
+++ idl_generator/generate.py
@@ -6,48 +6,64 @@ field_types = [
     ["bool", "byte", "i16", "i32", "i64", "float", "double", "string", "binary"],
     ["list", "set", "map", "struct"]
 ]
-struct_cache = []
 
-def generate_field_type(container_type_pct = 0.5):
-    complete_type = ""
-    base_or_container = 0 if random.random() > container_type_pct else 1
-    type_index = random.randint(0, len(field_types[base_or_container]) - 1)
+class StructGenerator:
+    def setThriftFile(self, thriftFile):
+        self.structCache = thriftFile.getStructCache()
+        self.f = thriftFile.getFile()
 
-    complete_type = field_types[base_or_container][type_index]
-    if (complete_type == "list" or complete_type == "set"):
-        complete_type += "<" + generate_field_type(container_type_pct / 2) + ">"
-    elif complete_type == "map":
-        complete_type += ("<" + generate_field_type(container_type_pct / 2) +
-        ", " + generate_field_type(container_type_pct / 2) + ">")
-    elif complete_type == "struct":
-        # Edge case when we can't generate structs
-        if len(struct_cache) == 0:
-            return "bool"
-        complete_type = struct_cache[random.randint(0, len(struct_cache) - 1)]
-    return complete_type
+    def generate_field_type(self, container_type_pct = 0.5):
+        complete_type = ""
+        base_or_container = 0 if random.random() > container_type_pct else 1
+        type_index = random.randint(0, len(field_types[base_or_container]) - 1)
 
-
-def generate_field(field_id):
-    field_string = "  " + str(field_id) + ": " + generate_field_type()
-    field_string += " field" + str(field_id)
-    print(field_string)
+        complete_type = field_types[base_or_container][type_index]
+        if (complete_type == "list" or complete_type == "set"):
+            complete_type += "<" + self.generate_field_type(container_type_pct / 2) + ">"
+        elif complete_type == "map":
+            complete_type += ("<" + self.generate_field_type(container_type_pct / 2) +
+            ", " + self.generate_field_type(container_type_pct / 2) + ">")
+        elif complete_type == "struct":
+            # Edge case when we can't generate structs
+            if len(self.structCache) == 0:
+                return "bool"
+            complete_type = self.structCache[random.randint(0, len(self.structCache) - 1)]
+        return complete_type
 
 
-def generate_struct(struct_id):
-    struct_name = "Struct" + str(struct_id)
-    print("struct " + struct_name + " {")
-    for field_id in range(0, int(np.random.gamma(1.2, 50))):
-        generate_field(field_id + 1)
-    print("}")
-    print("")
-    struct_cache.append(struct_name)
+    def generate_field(self, field_id):
+        field_string = "  " + str(field_id) + ": " + self.generate_field_type()
+        field_string += " field" + str(field_id)
+        print(field_string, file=self.f)
 
-def dump_to_file(file_name):
-    with io.open(file_name, 'w', encoding='utf-8') as f:
-        f.write(u"hey")
-        f.close()
+
+    def generate_struct(self, struct_id):
+        struct_name = "Struct" + str(struct_id)
+        print("struct " + struct_name + " {", file=self.f)
+        for field_id in range(0, int(np.random.gamma(1.2, 50))):
+            self.generate_field(field_id + 1)
+        print("}", file=self.f)
+        print("", file=self.f)
+        self.structCache.append(struct_name)
+
+class ThriftFile:
+    def __init__(self, file_num):
+        self.structCache = []
+        self.f = io.open("file" + str(file_num) + ".thrift", 'w', encoding='utf-8')
+
+    def closeFile(self):
+        self.f.close()
+
+    def getFile(self):
+        return self.f
+
+    def getStructCache(self):
+        return self.structCache
 
 if __name__ == "__main__":
-    # print(sorted(np.random.gamma(1.2, 50, 100).astype(int)))
+    struct_generator = StructGenerator()
+    thrift_file = ThriftFile(0)
+    struct_generator.setThriftFile(thrift_file)
     for struct_id in range(0, int(np.random.gamma(1.2, 50))):
-        generate_struct(struct_id)
+        struct_generator.generate_struct(struct_id)
+    thrift_file.closeFile()
