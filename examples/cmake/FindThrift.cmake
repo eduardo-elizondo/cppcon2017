c5311f330f9ac33c7e8136a10996a24aaa807b0f
eduardo-elizondo <eelizondo@fb.com>
2017-09-04 20:12:07 -0700

    Add CMakeFile to build examples

---
+++ examples/cmake/FindThrift.cmake
@@ -0,0 +1,30 @@
+#
+# - Try to find Facebook Thrift library
+# This will define
+# THRIFT_FOUND
+# THRIFT_INCLUDE_DIR
+# THRIFT_LIBRARY
+#
+
+find_path(
+  THRIFT_INCLUDE_DIR thrift/lib/cpp2/Thrift.h
+  HINTS
+      $ENV{THRIFT_ROOT}/include
+      ${THRIFT_ROOT}/include
+)
+
+find_library(
+    THRIFT_LIBRARY thriftcpp2
+    HINTS
+        $ENV{THRIFT_ROOT}/lib
+        ${THRIFT_ROOT}/lib
+)
+
+mark_as_advanced(THRIFT_INCLUDE_DIR THRIFT_LIBRARY)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(THRIFT THRIFT_INCLUDE_DIR THRIFT_LIBRARY)
+
+if(THRIFT_FOUND AND NOT THRIFT_FIND_QUIETLY)
+    message(STATUS "THRIFT: ${THRIFT_INCLUDE_DIR}")
+endif()
