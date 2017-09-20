71687772f45d256b1c227789318e3be40e16ffad
eduardo-elizondo <eelizondo@fb.com>
2017-09-20 14:46:58 -0700

    Mods

--- examples/build.sh
+++ examples/build.sh
@@ -1,5 +1,5 @@
 mkdir build
 pushd build
 cmake -Dtemplated=OFF -Dthrift_example_3=ON  -DTHRIFT_INCLUDE_DIR=~/Documents/fbthrift -DTHRIFT_LIBRARY=~/Documents/thrift_build/thrift/lib/cpp2 ..
-make
+make thrift_example_3_templated
 popd
