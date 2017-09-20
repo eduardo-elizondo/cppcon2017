e63c136f1d6a4c61d68550945853f53a949f41b3
eduardo-elizondo <eelizondo@fb.com>
2017-09-20 12:08:20 -0700

    Added cpp2

--- examples/build.sh
+++ examples/build.sh
@@ -1,5 +1,5 @@
 mkdir build
 pushd build
-cmake -Dtemplated=OFF -Dthrift_example_2=ON  -DTHRIFT_INCLUDE_DIR=~/Documents/fbthrift -DTHRIFT_LIBRARY=~/Documents/thrift_build/thrift/lib/cpp2 ..
+cmake -Dtemplated=OFF -Dthrift_example_3=ON  -DTHRIFT_INCLUDE_DIR=~/Documents/fbthrift -DTHRIFT_LIBRARY=~/Documents/thrift_build/thrift/lib/cpp2 ..
 make
 popd
