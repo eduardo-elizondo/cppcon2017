d05a5f4091d7ac3e6213e12f5d42cf7fffaaf361
eduardo-elizondo <eelizondo@fb.com>
2017-09-20 16:19:04 -0700

    Added inst-times

--- examples/build.sh
+++ examples/build.sh
@@ -1,5 +1,5 @@
 mkdir build
 pushd build
-cmake -Dtemplated=OFF -Dthrift_example_3=ON  -DTHRIFT_INCLUDE_DIR=~/Documents/fbthrift -DTHRIFT_LIBRARY=~/Documents/thrift_build/thrift/lib/cpp2 ..
-make thrift_example_3_templated
+cmake -Dtemplated=$1 -Dthrift_example_$2=ON  -DTHRIFT_INCLUDE_DIR=~/Documents/fbthrift -DTHRIFT_LIBRARY=~/Documents/thrift_build/thrift/lib/cpp2 ..
+make
 popd
