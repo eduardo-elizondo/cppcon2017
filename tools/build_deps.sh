f51da6eb50f27cf105021b103eb9d435a8a9f144
eduardo-elizondo <eelizondo@fb.com>
2017-09-20 22:27:33 -0700

    Improve deps script

--- tools/build_deps.sh
+++ tools/build_deps.sh
@@ -1,29 +1,30 @@
 # Get Deps and apply patches
+mkdir external
+pusd external
 svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-pushd llvm
+cd llvm
 svn patch ../patches/RefCount.diff
-pushd projects
+cd projects
 svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
 svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
-popd
-cd tools
+cd ../tools
 svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
-svn patch ../patches/IRTime.diff
-svn patch ../patches/LexingTime.diff
-svn patch ../patches/TemplateInstTime.diff
-cd clang/tools
+cd clang
+svn patch ../../../patches/IRTime.diff
+svn patch ../../../patches/LexingTime.diff
+svn patch ../../../patches/TemplateInstTime.diff
+cd tools
 git clone https://github.com/mikael-s-persson/templight
 cd ..
 svn patch tools/templight/templight_clang_patch.diff
 popd
 
 # Build llvm and clang
-mkdir llvm_build
-pushd llvm_build
+mkdir external/llvm_build
+pushd external/llvm_build
 cmake ../llvm \
   -G Ninja \
   -DCMAKE_BUILD_TYPE="Release" \
-  -DCMAKE_INSTALL_PREFIX=$HOME/local/install \
   -DLLVM_TARGETS_TO_BUILD="X86;AArch64;ARM" \
   -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-ldl,-lpthread,-lutil" \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
@@ -31,8 +32,8 @@ ninja
 popd
 
 # Get template instantiation time tools
+pushd external
 git clone https://github.com/eduardo-elizondo/templight-tools
-mkdir templight_tools
 cd templight_tools
-cmake ../templight-tools
+cmake .
 make -j 32
