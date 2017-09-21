a90c56177eb2e794f89bc5bbd9e36393a3f93c6d
eduardo-elizondo <eelizondo@fb.com>
2017-09-20 23:11:51 -0700

    Improve master cmake and build

--- examples/build.sh
+++ examples/build.sh
@@ -1,5 +1,5 @@
 mkdir build
 pushd build
-cmake -Dtemplated=$1 -Dthrift_example_$2=ON  -DTHRIFT_INCLUDE_DIR=~/Documents/fbthrift -DTHRIFT_LIBRARY=~/Documents/thrift_build/thrift/lib/cpp2 ..
+cmake -Dtemplated=$1 -Dexample$2=ON  -DTHRIFT_INCLUDE_DIR=~/Documents/fbthrift -DTHRIFT_LIBRARY=~/Documents/thrift_build/thrift/lib/cpp2 ..
 make
 popd
