e6ecbb26a6f11436fc3c4563060f95b3f14ac253
eduardo-elizondo <eelizondo@fb.com>
2017-09-24 17:43:40 -0700

    Added comments to build.sh

--- examples/build.sh
+++ examples/build.sh
@@ -1,5 +1,7 @@
 mkdir build
 pushd build
+#subdir is the directory you want to build: "fibonacci", "example1", ...
+#gen_type is for thrift examples. Either: "normal" or "templated"
 cmake -Dsubdir=$1 -Dgen_type=$2 ..
 make 2> $1_time-report.txt
 popd
