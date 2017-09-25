54d65c48f0b5f5ce968dc6d0498dfc64c30da219
eduardo-elizondo <eelizondo@fb.com>
2017-09-24 17:25:20 -0700

    Fix find script

--- tools/get_inst_times.sh
+++ tools/get_inst_times.sh
@@ -1,8 +1,8 @@
 templight-convert -f graphviz-cg -t 0.00005 -o $1_inst-time.txt $2
-find . -name "*inst-time.txt" | xargs sed -ie "/->/d;"
-find . -name "*inst-time.txt" | xargs perl -pi -e "s/^[0-9]*//d;"
-find . -name "*inst-time.txt" | xargs sed -ie "/digraph G {/d;"
-find . -name "*inst-time.txt" | xargs sed -ie "/}/d;"
+find . -maxdepth 1 -name "*inst-time.txt" | xargs sed -ie "/->/d;"
+find . -maxdepth 1 -name "*inst-time.txt" | xargs perl -pi -e "s/^[0-9]*//d;"
+find . -maxdepth 1 -name "*inst-time.txt" | xargs sed -ie "/digraph G {/d;"
+find . -maxdepth 1 -name "*inst-time.txt" | xargs sed -ie "/}/d;"
 rm $1_inst-time.txte
 sort -r $1_inst-time.txt > $1_inst-time.txt.tmp
 mv $1_inst-time.txt.tmp $1_inst-time.txt
